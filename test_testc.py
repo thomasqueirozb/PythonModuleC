9b1541b5a3a687163a85d4cf3ec6fd65c78dd8c8
Thomas Queiroz <thomasqueirozb@gmail.com>
2020-11-09 18:47:02 -0300

    Change test python file to work with modules that do not start with test

--- test_testc.py
+++ test_testc.py
@@ -1,27 +1,27 @@
 import testc
 
 
-def testc_print():
+def test_testc_print():
     assert testc.print() == None
 
 
-def testc_print_int():
+def test_testc_print_int():
     assert testc.print_int(123456) == None
 
 
-def testc_return_1():
+def test_testc_return_1():
     retval = testc.return_1()
     assert retval == 1
     assert type(retval) == int
 
 
-def testc_return_True():
+def test_testc_return_True():
     retval = testc.return_true()
     assert retval == True
     assert type(retval) == bool
 
 
-def testc_raise_testc_error():
+def test_testc_raise_testc_error():
     try:
         testc.raise_testc_error()
     except testc.TestcError:
